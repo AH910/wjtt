b42bc1db6a51f74c01b2d9a9eccb7b5428e09757
Andri Hardegger <andri@DESKTOP-D3ATLHE.localdomain>
2022-10-17 22:54:21 +0200

    No changes, just testing hooks.

--- waterbird_prep.py
+++ waterbird_prep.py
@@ -1,32 +1,25 @@
 import os
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, Subset
-from PIL import Image
-import numpy as np
+
 import pandas as pd
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset, Subset
 
 
 class WBDataset(Dataset):
-    
+
     # Custom dataset class for waterbird data
-    
-    def __init__(
-        self,
-        data_dir,
-        metadata_csv_name="metadata.csv"
-    ):
+
+    def __init__(self, data_dir, metadata_csv_name="metadata.csv"):
         self.data_dir = data_dir
 
         if not os.path.exists(self.data_dir):
             raise ValueError(
-                f"{self.data_dir} does not exist yet. Please generate the dataset first."
+                f"{self.data_dir} does not exist yet. Generate the dataset first."
             )
 
         # Read metadata
-        self.metadata_df = pd.read_csv(
-            os.path.join(self.data_dir, metadata_csv_name))
+        self.metadata_df = pd.read_csv(os.path.join(self.data_dir, metadata_csv_name))
 
         # Labels:   y == 0 --> Landbird
         #           y == 1 --> Waterbird
@@ -39,8 +32,9 @@ class WBDataset(Dataset):
         #           3 --> WB on water
         self.n_groups = 4
         self.confounder_array = self.metadata_df["place"].values
-        self.group_array = (self.y_array * (self.n_groups / 2) +
-                            self.confounder_array).astype("int")
+        self.group_array = (
+            self.y_array * (self.n_groups / 2) + self.confounder_array
+        ).astype("int")
 
         # Extract filenames and splits
         self.filename_array = self.metadata_df["img_filename"].values
@@ -55,12 +49,10 @@ class WBDataset(Dataset):
         self.train_transform = get_transform_wb(train=True)
         self.eval_transform = get_transform_wb(train=False)
 
-
     def __len__(self):
         return len(self.y_array)
 
-
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
         label = self.y_array[idx]
         group = self.group_array[idx]
 
@@ -74,18 +66,24 @@ class WBDataset(Dataset):
 
         return img, label, group, idx
 
-
     def split(self):
 
         # Splits the dataset into train, val and test data acc. to split_array
 
-        train_indices = [i for i, x in enumerate(self.split_array) if x == self.split_dict["train"]]
-        val_indices = [i for i, x in enumerate(self.split_array) if x == self.split_dict["val"]]
-        test_indices = [i for i, x in enumerate(self.split_array) if x == self.split_dict["test"]]
+        train_indices = [
+            i for i, x in enumerate(self.split_array) if x == self.split_dict["train"]
+        ]
+        val_indices = [
+            i for i, x in enumerate(self.split_array) if x == self.split_dict["val"]
+        ]
+        test_indices = [
+            i for i, x in enumerate(self.split_array) if x == self.split_dict["test"]
+        ]
 
-        return [Subset(self,indices) for indices in [train_indices,val_indices,test_indices]]
-            
-    
+        return [
+            Subset(self, indices)
+            for indices in [train_indices, val_indices, test_indices]
+        ]
 
 
 def get_transform_wb(train=True):
@@ -95,25 +93,31 @@ def get_transform_wb(train=True):
 
     if not train:
         # Resizes the image to a slightly larger square then crops the center.
-        transform = transforms.Compose([
-            transforms.Resize((
-                int(224 * scale),
-                int(224 * scale),
-            )),
-            transforms.CenterCrop((224,224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std),
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.Resize(
+                    (
+                        int(224 * scale),
+                        int(224 * scale),
+                    )
+                ),
+                transforms.CenterCrop((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
     else:
-        transform = transforms.Compose([
-            transforms.RandomResizedCrop(
-                (224,224),
-                scale=(0.7, 1.0),
-                ratio=(0.75, 1.3333333333333333),
-                interpolation=2,
-            ),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std),
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    (224, 224),
+                    scale=(0.7, 1.0),
+                    ratio=(0.75, 1.3333333333333333),
+                    interpolation=2,
+                ),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
     return transform
