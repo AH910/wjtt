1fb63a6eda950e9b5ff3c098867d024ebbb20e41
Andri Hardegger <andri@DESKTOP-D3ATLHE.localdomain>
2022-10-24 15:23:58 +0200

    Saving error set

--- jtt_first_model.py
+++ jtt_first_model.py
@@ -1,14 +1,15 @@
 import numpy as np
+import pandas as pd
 import torch
+import torch.nn as nn
 import torchvision.models as models
 from torch.utils.data import DataLoader
 
 import wandb
-from utils import hinge_loss
 from waterbird_prep import WBDataset
 
 # Login to wandb
-wandb.init(project="test_run")
+# wandb.init(project="test_run")
 
 # Set seed
 torch.manual_seed(0)
@@ -17,11 +18,15 @@ torch.manual_seed(0)
 device = torch.device("cuda")
 
 # Hyperparameters
-n_epochs = [30, 50, 70]
-batch_size = 64
+n_classes = 2
+n_epochs = [1]
+batch_size = 4
 lr = 1e-3
 weight_decay = 1.0
 model = models.resnet50()
+
+d = model.fc.in_features
+model.fc = nn.Linear(d, n_classes)
 
 # Loading full waterbird dataset
 full_dataset = WBDataset(
@@ -38,8 +43,11 @@ train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(val_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
+# Send model weights to GPU
+# model = model.to(device)
+
 # Loss and optimizer
-criterion = hinge_loss
+criterion = nn.CrossEntropyLoss(reduction="none")
 optimizer = torch.optim.SGD(
     filter(lambda p: p.requires_grad, model.parameters()),
     lr=lr,
@@ -53,13 +61,14 @@ for epoch in range(max(n_epochs)):
     # Training
     train_loss = 0.0
     train_correct_pred = 0.0
+    error_set = np.array([])
+
+    # Put model in training mode
+    # model.train()
 
     for batch_idx, batch in enumerate(train_dataloader):
 
-        # Put model in training mode
-        model.train()
-
-        batch = tuple(t.to(device) for t in batch)
+        # batch = tuple(t.to(device) for t in batch)
         x = batch[0]
         y = batch[1]
         g = batch[2]
@@ -70,16 +79,29 @@ for epoch in range(max(n_epochs)):
         loss = criterion(outputs, y)
 
         # Backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # optimizer.zero_grad()
+        # loss.mean().backward()
+        # optimizer.step()
 
         # Add loss of this batch to total and add correctly predicted samples to total
-        train_loss += loss.item()
+        train_loss += loss.mean().item()
         y_pred = np.argmax(outputs.detach().cpu().numpy(), axis=1)
         y_true = y.cpu().numpy()
+        if batch_idx % 100 == 0:
+            print(f"data_idx: {data_idx}")
+            print(f"outputs: {outputs.detach().numpy()}")
+            print(f"y_pred: {y_pred}")
+            print(f"y_true: {y_true}")
+
         train_correct_pred += np.sum(y_pred == y_true)
 
+        if epoch + 1 in n_epochs:
+            indices = []
+            for k in range(len(data_idx)):
+                if y_true[k] != y_pred[k]:
+                    indices.append(data_idx[k].cpu())
+            error_set = np.append(error_set, indices)
+    break
     # Validation
     val_loss = 0.0
     val_correct_pred = 0.0
@@ -99,7 +121,7 @@ for epoch in range(max(n_epochs)):
         loss = criterion(outputs, y)
 
         # Add loss of this batch to total and add correctly predicted samples to total
-        val_loss += loss.item()
+        val_loss += loss.mean().item()
         y_pred = np.argmax(outputs.detach().cpu().numpy(), axis=1)
         y_true = y.cpu().numpy()
         val_correct_pred += np.sum(y_pred == y_true)
@@ -116,11 +138,23 @@ for epoch in range(max(n_epochs)):
     )
 
     if epoch + 1 in n_epochs:
-        torch.save(
-            model.state_dict(),
-            "./models/jtt/first_models/resnet50_"
+        # Save error set
+        error_set.sort()
+        pd.DataFrame(error_set).to_csv(
+            "./results/jtt/waterbird/error_sets/"
             + f"nepochs_{epoch+1}_"
             + f"lr_{lr}_"
             + f"batch_size_{batch_size}_"
-            + f"wd_{weight_decay}",
+            + f"wd_{weight_decay}.csv",
+            header=None,
+            index=None,
         )
+        # # Save model
+        # torch.save(
+        #     model.state_dict(),
+        #     "./models/jtt/first_models/resnet50_"
+        #     + f"nepochs_{epoch+1}_"
+        #     + f"lr_{lr}_"
+        #     + f"batch_size_{batch_size}_"
+        #     + f"wd_{weight_decay}",
+        # )
