1f43ba6c6f9e2463e07f90e950dcb31bd936ce22
Andri Hardegger <andri@DESKTOP-D3ATLHE.localdomain>
2022-10-26 12:23:50 +0200

    Upweighting samples in error set in training data.

--- jtt_second_model.py
+++ jtt_second_model.py
@@ -1,7 +1,9 @@
+import numpy as np
 import torch
-import torchvision.models as models
+from torch.utils.data import ConcatDataset, DataLoader, Subset
 
 import wandb
+from waterbird_prep import WBDataset
 
 # Hyperparameters first model
 n_epochs_1 = 50
@@ -17,7 +19,7 @@ weight_decay_2 = 1.0
 upweight = 20
 
 # Login to wandb
-wandb.init(project="test")
+wandb.init(project="JTT (2nd model)")
 
 # Set seed
 torch.manual_seed(0)
@@ -25,18 +27,33 @@ torch.manual_seed(0)
 # Device configuration
 device = torch.device("cuda")
 
-# Load first model
-model = models.resnet50()
-model.load_state_dict(
-    torch.load(
-        "./models/jtt/first_models/waterbird/resnet50_"
-        + f"nepochs_{n_epochs_1}_"
-        + f"lr_{lr_1}_"
-        + f"batch_size_{batch_size_1}_"
-        + f"wd_{weight_decay_1}"
-    )
+# Load full dataset
+full_dataset = WBDataset(
+    data_dir="./data/waterbird_complete95_forest2water2",
+    metadata_csv_name="metadata.csv",
 )
-model.eval()
 
-# TODO: Load data and upweight the samples incorrectly classified
-# by the first model
+# Split dataset
+train_data, val_data, test_data = full_dataset.split()
+
+# Load error set from first model
+error_set = np.genfromtxt(
+    "./results/jtt/waterbird/error_sets/"
+    + f"nepochs_{n_epochs_1}_"
+    + f"lr_{lr_1}_"
+    + f"batch_size_{batch_size_1}_"
+    + f"wd_{weight_decay_1}.csv",
+    delimiter=",",
+)
+
+# Add the samples, that were incorrectly classified by the 1st model, (upweight-1) times
+# to the training data, so that every sample that was correctly classified is in the
+# training data once and every sample in the error set is in the training data
+# (upweight) times
+upweighted_samples = Subset(full_dataset, list(error_set) * (upweight - 1))
+train_data = ConcatDataset([train_data, upweighted_samples])
+
+# Split dataset and define dataloaders and sampler for trainloader
+train_dataloader = DataLoader(train_data, batch_size=batch_size_2)
+val_dataloader = DataLoader(val_data, batch_size=batch_size_2)
+test_dataloader = DataLoader(test_data, batch_size=batch_size_2)
