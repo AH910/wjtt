25c53ff3264dab6f29aabeab58483696ceb621d6
Andri Hardegger <andri@DESKTOP-D3ATLHE.localdomain>
2023-02-19 20:48:45 +0100

    Final commit.

--- plot_ESS_vs_unif.py
+++ plot_ESS_vs_unif.py
@@ -4,90 +4,111 @@ import numpy as np
 from data import dataset_attributes
 from utils import get_weights
 
+# Script for generating Figure 6
+# Takes about 12min
 
+# Set font for plot Computer Modern (LaTex font)
+plt.rcParams["font.family"] = "cmr10"
+# Minus sign in CM font
+plt.rcParams["axes.unicode_minus"] = False
+
+# Returns the effective sample size for a set of weights.
 def get_eff_sample_size(weights):
     return (sum(weights) ** 2) / sum(w**2 for w in weights)
 
 
-dataset = "celebA"
-dataset_attributes = dataset_attributes[dataset]
-n_groups = dataset_attributes["n_groups"]
-
-# Log(min(rel. sum of weights)) or Log(prod(rel. sum of weights))
-m_or_p = "p"
-
-#
+# Which functions to compare
 weighting = ["JTT", "DRO2", "w1", "w2"]
+
+# Hyperparameter for different methods
 alphas = {
-    "JTT": [25, 50, 75, 100, 125, 150, 175],
-    "DRO2": [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100],
-    "w1": [0.5, 0.1, 0.01, 0.001, 0.0001],
-    "w2": [4, 6, 8, 10, 12, 14],
+    "JTT": np.linspace(0, 20, 101),
+    "DRO2": np.linspace(0, 15, 101),
+    "w1": np.linspace(0, 4, 101),
+    "w2": np.linspace(0, 2, 101),
 }
-weighting = ["JTT", "DRO2", "w1", "w2"]
+rho = 2
 
-# Load data and error set
-error_set = {
-    "waterbird": "./error_sets/waterbird/nepochs_60_lr_1e-05_batch_size_64_wd_1.0.csv",
-    "celebA": "./error_sets/celebA/nepochs_1_lr_1e-05_batch_size_64_wd_0.1.csv",
-    "CivilComments": "./error_sets/CivilComments/nepochs_2_lr_1e-05_batch_size_16_wd_0.01.csv",
-    "MultiNLI": "./error_sets/MultiNLI/",
-}
-
-indices, probs = list(
-    np.genfromtxt(
-        error_set[dataset],
-        delimiter=",",
-    )
-)
-indices = [int(x) for x in indices]
-full_dataset = dataset_attributes["class"]()
-groups = full_dataset.group_array[indices]
-
+# Initialize dict. for ESS stats
 ESS = {
-    "JTT": [],
-    "DRO2": [],
-    "w1": [],
-    "w2": [],
+    "waterbird": {
+        "JTT": [],
+        "DRO2": [],
+        "w1": [],
+        "w2": [],
+    },
+    "celebA": {
+        "JTT": [],
+        "DRO2": [],
+        "w1": [],
+        "w2": [],
+    },
 }
 
+# Initialize dictionary for uniformity stats
 uniformity = {
-    "JTT": [],
-    "DRO2": [],
-    "w1": [],
-    "w2": [],
+    "waterbird": {
+        "JTT": [],
+        "DRO2": [],
+        "w1": [],
+        "w2": [],
+    },
+    "celebA": {
+        "JTT": [],
+        "DRO2": [],
+        "w1": [],
+        "w2": [],
+    },
 }
 
-#
+for dataset in ["waterbird", "celebA"]:
+    # Load dataset to get group labels
+    dataset_attrs = dataset_attributes[dataset]
+    full_dataset = dataset_attrs["class"]()
+    train_data, val_data, test_data = full_dataset.split()
+    groups = [train_data[k][2] for k in range(len(train_data))]
+    n_groups = dataset_attrs["n_groups"]
+
+    # Load error_set
+    error_set = "./error_sets/" + dataset + "/err_set.csv"
+    probs = list(np.genfromtxt(error_set, delimiter=","))
+
+    # Go through all functions and parameters and calculate ESS and uniformity
+    for func in weighting:
+        for alpha in alphas[func]:
+            # get weights and calculate rel. weight of groups
+            weights = get_weights(probs, alpha, rho, func)
+            g_weights = [[] for k in range(n_groups)]
+            for k in range(len(weights)):
+                g_weights[groups[k]].append(weights[k])
+            rel_weights = [sum(g_weights[k]) / sum(weights) for k in range(n_groups)]
+
+            # Calculate effective sample size
+            ESS[dataset][func].append(np.log(get_eff_sample_size(weights)))
+
+            # Append product of rel. group sizes
+            uniformity[dataset][func].append(np.log(np.prod(rel_weights)))
+
+# Make plot
+fig, axs = plt.subplots(1, 2)
+ylabel = "Log(product)"
+xlabel = "Log(ESS)"
+
+# waterbird plot
 for func in weighting:
-    for alpha in alphas[func]:
-        # get weights and calculate rel. weight of groups
-        weights = get_weights(probs, alpha, func)
-        g_weights = [[] for k in range(n_groups)]
-        for k in range(len(weights)):
-            g_weights[groups[k]].append(weights[k])
-        rel_weights = [sum(g_weights[k]) / sum(weights) for k in range(n_groups)]
+    axs[0].plot(ESS["waterbird"][func], uniformity["waterbird"][func], label=func)
+axs[0].title.set_text("waterbird")
+axs[0].set_ylabel(ylabel)
+axs[0].set_xlabel(xlabel)
+axs[0].legend()
 
-        # Calculate effective sample size
-        ESS[func].append(np.log(get_eff_sample_size(weights)))
-
-        # Calculate uniformity
-        if m_or_p == "m":
-            uniformity[func].append(np.log(min(rel_weights)))
-        else:
-            uniformity[func].append(np.log(np.prod(rel_weights)))
-
-
-# Labels of plot
-if m_or_p == "m":
-    ylabel = "Log(min(rel. sum of weights))"
-elif m_or_p == "p":
-    ylabel = "Log(prod(rel. sum of weights))"
-
+# celebA plot
 for func in weighting:
-    plt.plot(ESS[func], uniformity[func], label=func)
-plt.title(dataset)
-plt.xlabel("Log(eff_sample_size)")
-plt.ylabel(ylabel)
-plt.legend()
-plt.savefig(f"{dataset}_" + m_or_p, dpi=300)
+    axs[1].plot(ESS["celebA"][func], uniformity["celebA"][func], label=func)
+axs[1].title.set_text("celebA")
+axs[1].set_xlabel(xlabel)
+axs[1].legend()
+
+# Save plot
+plt.savefig("ESS_vs_prod.png", dpi=300)
+plt.savefig("ESS_vs_prod.eps", dpi=300)
